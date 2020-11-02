47dd795fb3d02b402ab6e8a8dd36814198be5c7d
Katie Bridson <kbridson@memphis.edu>
2020-11-02 07:01:26 -0600

    Added proper syntax highlighting

---
+++ assets/js/hljs-ruby.js
@@ -0,0 +1,262 @@
+/*
+Language: Ruby
+Description: Ruby is a dynamic, open source programming language with a focus on simplicity and productivity.
+Website: https://www.ruby-lang.org/
+Author: Anton Kovalyov <anton@kovalyov.net>
+Contributors: Peter Leonov <gojpeg@yandex.ru>, Vasily Polovnyov <vast@whiteants.net>, Loren Segal <lsegal@soen.ca>, Pascal Hurni <phi@ruby-reactive.org>, Cedric Sohrauer <sohrauer@googlemail.com>
+Category: common
+*/
+
+hljs.registerLanguage("ruby", function() {
+  "use strict";
+  return function(hljs) {
+    var RUBY_METHOD_RE = '[a-zA-Z_]\\w*[!?=]?|[-+~]\\@|<<|>>|=~|===?|<=>|[<>]=?|\\*\\*|[-/+%^&*~`|]|\\[\\]=?';
+    var RUBY_KEYWORDS = {
+      // $pattern: /-[a-z]+/,        // allow keywords to begin with dash
+      keyword:
+        'if when then module in return redo BEGIN retry end for next until do begin unless END rescue else break undef super class case yield alias while ensure elsif',
+      literal:
+        'true false nil',
+      operator:
+        'or and not',
+      // variable:
+      //   'get post put patch delete render respond_to before_action redirect_to format.html params',
+      keyword_special:
+        'require include attr_reader attr_writer attr_accessor ',
+      class:
+        'self'
+    };
+    var YARDOCTAG = {
+      className: 'doctag',
+      begin: '@[A-Za-z]+'
+    };
+    var IRB_OBJECT = {
+      begin: '#<', end: '>'
+    };
+    var COMMENT_MODES = [
+      hljs.COMMENT(
+        '#',
+        '$',
+        {
+          contains: [YARDOCTAG]
+        }
+      ),
+      hljs.COMMENT(
+        '^\\=begin',
+        '^\\=end',
+        {
+          contains: [YARDOCTAG],
+          relevance: 10
+        }
+      ),
+      hljs.COMMENT('^__END__', '\\n$')
+    ];
+    var SUBST = {
+      className: 'subst',
+      begin: '#\\{', end: '}',
+      keywords: RUBY_KEYWORDS
+    };
+    var STRING = {
+      className: 'string',
+      contains: [hljs.BACKSLASH_ESCAPE, SUBST],
+      variants: [
+        {begin: /'/, end: /'/},
+        {begin: /"/, end: /"/},
+        {begin: /`/, end: /`/},
+        {begin: '%[qQwWx]?\\(', end: '\\)'},
+        {begin: '%[qQwWx]?\\[', end: '\\]'},
+        {begin: '%[qQwWx]?{', end: '}'},
+        {begin: '%[qQwWx]?<', end: '>'},
+        {begin: '%[qQwWx]?/', end: '/'},
+        {begin: '%[qQwWx]?%', end: '%'},
+        {begin: '%[qQwWx]?-', end: '-'},
+        {begin: '%[qQwWx]?\\|', end: '\\|'},
+        {
+          // \B in the beginning suppresses recognition of ?-sequences where ?
+          // is the last character of a preceding identifier, as in: `func?4`
+          begin: /\B\?(\\\d{1,3}|\\x[A-Fa-f0-9]{1,2}|\\u[A-Fa-f0-9]{4}|\\?\S)\b/
+        },
+        { // heredocs
+          begin: /<<[-~]?'?(\w+)(?:.|\n)*?\n\s*\1\b/,
+          returnBegin: true,
+          contains: [
+            { begin: /<<[-~]?'?/ },
+            hljs.END_SAME_AS_BEGIN({
+              begin: /(\w+)/, end: /(\w+)/,
+              contains: [hljs.BACKSLASH_ESCAPE, SUBST],
+            })
+          ]
+        }
+      ]
+    };
+
+    var VARIABLE = {
+      className: 'variable',
+      begin: /(\$\W)|((\$|\@\@?)(\w+))|\b(?<!:|\.)([a-z]\w*)(?!:)\b/,
+      keywords: RUBY_KEYWORDS
+    }
+
+    var FUNCTION = {
+      className: 'function',
+      begin: /\b(?<=\.)([a-z]\w*\b\??)/,
+      keywords: RUBY_KEYWORDS
+    }
+
+    var CLASS = {
+      className: 'class',
+      begin: /\b([A-Z]\w*)\b/,
+      keywords: RUBY_KEYWORDS
+    }
+
+    var VARIABLE_OTHER = {
+      className: 'variable',
+      keywords: 'get post put patch delete render respond_to'
+    }
+  
+    // Ruby syntax is underdocumented, but this grammar seems to be accurate
+    // as of version 2.7.2 (confirmed with (irb and `Ripper.sexp(...)`)
+    // https://docs.ruby-lang.org/en/2.7.0/doc/syntax/literals_rdoc.html#label-Numbers
+    var decimal = '[1-9](_?[0-9])*|0';
+    var digits = '[0-9](_?[0-9])*';
+    var NUMBER = {
+      className: 'number', relevance: 0,
+      variants: [
+        // decimal integer/float, optionally exponential or rational, optionally imaginary
+        { begin: `\\b(${decimal})(\\.(${digits}))?([eE][+-]?(${digits})|r)?i?\\b` },
+  
+        // explicit decimal/binary/octal/hexadecimal integer,
+        // optionally rational and/or imaginary
+        { begin: "\\b0[dD][0-9](_?[0-9])*r?i?\\b" },
+        { begin: "\\b0[bB][0-1](_?[0-1])*r?i?\\b" },
+        { begin: "\\b0[oO][0-7](_?[0-7])*r?i?\\b" },
+        { begin: "\\b0[xX][0-9a-fA-F](_?[0-9a-fA-F])*r?i?\\b" },
+  
+        // 0-prefixed implicit octal integer, optionally rational and/or imaginary
+        { begin: "\\b0(_?[0-7])+r?i?\\b" },
+      ]
+    };
+  
+    var PARAMS = {
+      className: 'params',
+      begin: '\\(', end: '\\)', endsParent: true,
+      keywords: RUBY_KEYWORDS
+    };
+
+    var operators = '([+-!/*]=?)|\\?';
+  
+    var RUBY_DEFAULT_CONTAINS = [
+      STRING,
+      IRB_OBJECT,
+      {
+        className: 'class',
+        beginKeywords: 'class module', end: '$|;',
+        illegal: /=/,
+        contains: [
+          hljs.inherit(
+            hljs.TITLE_MODE, 
+            {
+              className: 'module',
+              begin: '[A-Za-z_]\\w*(::\\w+)*(\\?|\\!)?'}
+            ),
+          {
+            className: 'inherited-class',
+            begin: '<\\s*',
+            contains: [{
+              begin: '(' + hljs.IDENT_RE + '::)?' + hljs.IDENT_RE
+            }]
+          }
+        ].concat(COMMENT_MODES)
+      },
+      {
+        className: 'function',
+        beginKeywords: 'def', end: '$|;',
+        contains: [
+          hljs.inherit(hljs.TITLE_MODE, {begin: RUBY_METHOD_RE}),
+          PARAMS
+        ].concat(COMMENT_MODES)
+      },
+      {
+        // swallow namespace qualifiers before symbols
+        begin: hljs.IDENT_RE + '::'
+      },
+      {
+        className: 'symbol',
+        begin: hljs.UNDERSCORE_IDENT_RE + '(\\!|\\?)?:',
+        relevance: 0
+      },
+      {
+        className: 'symbol',
+        begin: ':(?!\\s)',
+        contains: [STRING, {begin: RUBY_METHOD_RE}],
+        relevance: 0
+      },
+      NUMBER,
+      VARIABLE,
+      FUNCTION,
+      CLASS,
+      {
+        // className: 'variable',
+        begin: '(\\$\\W)|((\\$|\\@\\@?)(\\w+))' // variables
+      },
+      {
+        // className: 'params',
+        begin: /\|/, end: /\|/,
+        contains: [
+          VARIABLE
+        ],
+        keywords: RUBY_KEYWORDS
+      },
+      { // regexp container
+        begin: '(' + hljs.RE_STARTERS_RE + '|unless)\\s*',
+        keywords: 'unless',
+        contains: [
+          IRB_OBJECT,
+          {
+            className: 'regexp',
+            contains: [hljs.BACKSLASH_ESCAPE, SUBST],
+            illegal: /\n/,
+            variants: [
+              {begin: '/', end: '/[a-z]*'},
+              {begin: '%r{', end: '}[a-z]*'},
+              {begin: '%r\\(', end: '\\)[a-z]*'},
+              {begin: '%r!', end: '![a-z]*'},
+              {begin: '%r\\[', end: '\\][a-z]*'}
+            ]
+          }
+        ].concat(COMMENT_MODES),
+        relevance: 0
+      }
+    ].concat(COMMENT_MODES);
+  
+    SUBST.contains = RUBY_DEFAULT_CONTAINS;
+    PARAMS.contains = RUBY_DEFAULT_CONTAINS;
+  
+    var SIMPLE_PROMPT = "[>?]>";
+    var DEFAULT_PROMPT = "[\\w#]+\\(\\w+\\):\\d+:\\d+>";
+    var RVM_PROMPT = "(\\w+-)?\\d+\\.\\d+\\.\\d(p\\d+)?[^>]+>";
+  
+    var IRB_DEFAULT = [
+      {
+        begin: /^\s*=>/,
+        starts: {
+          end: '$', contains: RUBY_DEFAULT_CONTAINS
+        }
+      },
+      {
+        className: 'meta',
+        begin: '^('+SIMPLE_PROMPT+"|"+DEFAULT_PROMPT+'|'+RVM_PROMPT+')',
+        starts: {
+          end: '$', contains: RUBY_DEFAULT_CONTAINS
+        }
+      }
+    ];
+  
+    return {
+      name: 'Ruby',
+      aliases: ['rb', 'gemspec', 'podspec', 'thor', 'irb'],
+      keywords: RUBY_KEYWORDS,
+      illegal: /\/\*/,
+      contains: COMMENT_MODES.concat(IRB_DEFAULT).concat(RUBY_DEFAULT_CONTAINS)
+    };
+  }
+}());
